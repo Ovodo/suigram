0937b6a7e1d8832c32e083cfae7171707d48df3e
hiWisdom <akpotororodje@gmail.com>
2024-05-17 06:00:13 +0100

    gram profile development

---
+++ components/app-profile/All.tsx
@@ -0,0 +1,32 @@
+
+
+
+
+
+import React, {useState} from 'react';
+import {View,Text,StyleSheet} from 'react-native';
+
+
+const All = () => {
+
+
+   return (
+
+    <>
+        <View>
+            <Text>This is All category</Text>
+        </View>
+    </>
+ 
+
+  );
+
+};
+
+export default All;
+
+
+const styles = StyleSheet.create({
+
+
+});
