d76e9f1832b570e32faea320dd4583220457b85f
hiWisdom <akpotororodje@gmail.com>
2024-05-23 08:36:31 +0100

    sui gram upadte

---
+++ assets/icons/images.js
@@ -0,0 +1,90 @@
+import * as React from "react"
+
+
+const MutiSvgComponent = (props) => (
+  
+  
+//   <svg
+//     xmlns="http://www.w3.org/2000/svg"
+//     width={18}
+//     height={18}
+//     fill="none"
+//     {...props}
+//   >
+//     <path
+//       stroke="#666"
+//       strokeLinecap="round"
+//       strokeLinejoin="round"
+//       strokeWidth={1.5}
+//       d="M8.358 13.412H3.503"
+//     />
+//     <path
+//       stroke="#666"
+//       strokeLinecap="round"
+//       strokeLinejoin="round"
+//       strokeWidth={1.5}
+//       d="M11.404 13.413c0 1.531.51 2.041 2.041 2.041 1.53 0 2.041-.51 2.041-2.041s-.51-2.041-2.041-2.041c-1.53 0-2.041.51-2.041 2.04Z"
+//       clipRule="evenodd"
+//     />
+//     <path
+//       stroke="#666"
+//       strokeLinecap="round"
+//       strokeLinejoin="round"
+//       strokeWidth={1.5}
+//       d="M10.632 5.546h4.854"
+//     />
+//     <path
+//       stroke="#666"
+//       strokeLinecap="round"
+//       strokeLinejoin="round"
+//       strokeWidth={1.5}
+//       d="M7.586 5.545c0-1.53-.51-2.042-2.04-2.042-1.532 0-2.043.511-2.043 2.042 0 1.531.51 2.041 2.042 2.041 1.53 0 2.041-.51 2.041-2.041Z"
+//       clipRule="evenodd"
+//     />
+//   </svg>
+
+
+
+
+
+
+
+
+<svg
+xmlns="http://www.w3.org/2000/svg"
+width={18}
+height={18}
+fill="none"
+{...props}
+>
+<g filter="url(#filter0_d_352_966)">
+<rect x="4.93005" y="0.768555" width={8.56471} height={8.51132} rx={2} fill="white"/>
+<path d="M16.2199 9.60115V3.6738C16.2199 3.35129 15.9584 3.08984 15.6359 3.08984C15.3134 3.08984 15.052 3.35129 15.052 3.6738V8.44052C15.052 9.54509 14.1565 10.4405 13.052 10.4405H8.23547C7.91497 10.4405 7.65515 10.7003 7.65515 11.0208C7.65515 11.3413 7.91497 11.6011 8.23547 11.6011H14.2199C15.3244 11.6011 16.2199 10.7057 16.2199 9.60115Z" fill="white"/>
+</g>
+<defs>
+<filter id="filter0_d_352_966" x="0.930054" y="0.768555" width="19.2898" height="18.8325" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset dy="4"/>
+<feGaussianBlur stdDeviation="2"/>
+<feComposite in2="hardAlpha" operator="out"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_352_966"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_352_966" result="shape"/>
+</filter>
+</defs>
+</svg>
+
+
+
+
+
+
+
+
+
+
+
+
+)
+export default MutiSvgComponent
